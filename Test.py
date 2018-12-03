f3db23384367f5d3ab0f6fe75ca111c824086a40
Tamim <tamim.365.ti@gmail.com>
2018-12-04 02:08:44 +0600

    Turtle try v1.0

--- Test.py
+++ Test.py
@@ -1,1 +1,24 @@
-print('Hello, world!')
+import turtle
+
+wn_bg=input("Input pop-up windows color: ")
+tess_color=input("Input tess color: ")
+pen_size=int(input("Input pen size: "))
+
+wn=turtle.Screen()
+wn.bgcolor(wn_bg)
+wn.title("Hello, Tess! ")
+
+tess=turtle.Turtle()
+tess.color(tess_color)
+tess.pensize(pen_size)
+
+tess.forward(120)
+tess.left(90)
+tess.forward(60)
+tess.left(90)
+tess.forward(120)
+tess.left(90)
+tess.forward(60)
+
+
+wn.mainloop()
